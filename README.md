65211ec89b57a62c7dc2e2c38a7222630361e21a
Rubén <hey@rubenvara.io>
2021-01-22 03:22:50 +0100

    start project 🚀🚀

---
+++ README.md
@@ -0,0 +1,1 @@
+# FreeCodeCamp Exercise Tracker
---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
